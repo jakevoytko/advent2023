10cf229741c0751a01f7a4321856915bb7bf0430
Jacob Voytko <jakevoyko@gmail.com>
2023-12-09 09:29:01 -0500

    Day 9

---
+++ 09/01.py
@@ -0,0 +1,23 @@
+from functools import reduce
+f = open('input.txt', 'r')
+
+sum = 0
+
+for line in f:
+    numbers = [int(atom) for atom in line.strip().split(' ')]
+    
+    rows = [numbers]
+    while not reduce(lambda a, b: a and (b == 0), rows[-1], True):
+        newrow = []
+        for i in range(len(rows[-1]) - 1):
+            newrow.append(rows[-1][i + 1] - rows[-1][i])
+        rows.append(newrow)
+
+    val = 0
+    for i in range(len(rows) - 1, -1, -1):
+        val += rows[i][-1]
+    sum += val
+
+print(sum)
+
+f.close()
